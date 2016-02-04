1e4d03ebadbf647810f5f78bb4fc9dfce572ddc6
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-02-04 21:59:37 +0100

    Bump version

--- lib/AtteanX/Query/AccessPlan/LDF.pm
+++ lib/AtteanX/Query/AccessPlan/LDF.pm
@@ -7,7 +7,7 @@ package AtteanX::Query::AccessPlan::LDF;
 use Class::Method::Modifiers;
 
 our $AUTHORITY = 'cpan:KJETILK';
-our $VERSION   = '0.005_03';
+our $VERSION   = '0.006';
 
 use Moo::Role;
 use Carp;
--- lib/AtteanX/Store/LDF.pm
+++ lib/AtteanX/Store/LDF.pm
@@ -44,7 +44,7 @@ use warnings;
 
 package AtteanX::Store::LDF;
 
-our $VERSION = '0.005_03';
+our $VERSION = '0.006';
 
 use Moo;
 use Attean::API::Store;
